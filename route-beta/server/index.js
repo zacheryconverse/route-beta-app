7e26894f270c5edf5dca6358e212231c475267ff
Zachery Converse <zacheryconverse@gmail.com>
2020-07-25 12:08:56 -0600

    Build GET route for /items. Add proxy: 3001 to package.json.

--- route-beta/server/index.js
+++ route-beta/server/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const items = require('./database/index.js');
+var items = require('./database/index');
 
-const app = express();
+var app = express();
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static('public'));
 
-router.get('/items', (req, res) => {
-  res.json([
-    {id: 1, username: 'somebody'},
-    {id: 2, username: 'somebody-else'}
-  ]);
-});
-
-// app.get('/items', function (req, res) {
-//   items.find(function(err, data) {
-//     if(err) {
-//       res.sendStatus(500);
-//     } else {
-//       res.json(data);
-//     }
-//   });
+// router.get('/items', (req, res) => {
+//   res.json([
+//     {id: 1, username: 'somebody'},
+//     {id: 2, username: 'somebody-else'}
+//   ]);
 // });
 
-app.listen(3002, function() {
-  console.log('listening on port 3002');
+app.get('/items', function (req, res) {
+  items.selectAll(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.listen(3001, function() {
+  console.log('listening on port 3001');
 });
