8cc964a788bf640b860e47b32e27a1681fa8c40d
Zachery Converse <zacheryconverse@gmail.com>
2020-07-25 11:37:58 -0600

    Set up basic structure.

--- route-beta/src/App.js
+++ route-beta/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import ReactDOM from 'react-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header.jsx";
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("/items")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          items: data,
+        });
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <header className="App-header">
+          <List items={this.state.items}/>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
