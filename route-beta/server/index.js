7f7caa78fe670a887d8f6a5dd67891d06fa92f90
Zachery Converse <zacheryconverse@gmail.com>
2020-07-27 14:54:47 -0600

    prepare for deploy.

--- route-beta/server/index.js
+++ route-beta/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const moves = require('./routes/api/moves');
 
@@ -14,6 +15,16 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
+
+// Serve static assets of in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 3001;
 
