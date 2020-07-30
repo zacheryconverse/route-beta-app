43a96baef7f19dffc9f8662182193efdcdb804ae
Zachery Converse <zacheryconverse@gmail.com>
2020-07-29 18:28:49 -0600

    Add authentication of existing users/login component.

--- route-beta/src/components/NavBar.jsx
+++ route-beta/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand, Container, NavbarToggler, Collapse, Nav, NavItem } from 'reactstrap';
 import RegisterModal from './auth/RegisterModal';
+import LoginModal from './auth/LoginModal';
 import Logout from './auth/Logout';
 
 class NavBar extends Component {
@@ -21,6 +22,7 @@ class NavBar extends Component {
           <Container>
             <NavbarBrand href="/">Route Beta</NavbarBrand>
             <RegisterModal />
+            <LoginModal />
             <Logout />
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
