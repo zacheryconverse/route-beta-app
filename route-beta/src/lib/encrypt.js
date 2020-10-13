3570c155142316e3c09901f18bb359023890e622
Zachery Converse <zacheryconverse@gmail.com>
2020-10-13 08:42:42 -0600

    Generate and derive key - encrypt and decrypt.

---
+++ route-beta/src/lib/encrypt.js
@@ -0,0 +1,25 @@
+export default async (messageJSON, derivedKey) => {
+  try {
+    const message = JSON.parse(messageJSON);
+    const text = message.base64Data;
+    const initializationVector = new Unit8Array(message.initializationVector).buffer;
+
+    const string atob(text);
+    const uintArray = New Unit8Array(
+      [...string].map((char) => char.charCodeAt(0))
+    )
+    const algorithm = {
+      name: "AES-GCM",
+      iv: initializationVector,
+    };
+    const decryptedData = await window.crypto.subtle.decrypt(
+      algorithm,
+      derivedKey,
+      uintArray
+    );
+
+    return new TextDecoder().decode(decryptedData);
+  } catch (e) {
+    return `error decrypting message: ${e}`;
+  }
+};
---
+++ route-beta/src/lib/generateKeyPair.js
@@ -0,0 +1,16 @@
+export default async () => {
+  const keyPair = await window.crypto.subtle.generateKey(
+    {
+      name: 'ECDH',
+      namedCurve: 'P-256',
+    },
+    true,
+    ['deriveKey', 'deriveBits']
+  );
+
+  const publicKeyJwk = await window.crypto.subtle.exportKey('jwk', keyPair.publicKey);
+
+  const privateKeyJwk = await window.crypto.subtle.exportKey('jwk', keyPair.privateKey);
+
+  return { publicKeyJwk, privateKey };
+};
